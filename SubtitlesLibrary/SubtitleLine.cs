976f36f2b95dbb0c7ecccf096b521f7495acf9b9
smallbits <mircamilanov95@gmail.com>
2018-08-07 14:30:04 +0200

    SubtitleLine base implementation

---
+++ SubtitlesLibrary/SubtitleLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitlesLibrary
+{
+    class SubtitleLine
+    {
+        public bool HasLineNumber { get; set; }
+        public int LineNumber { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Text { get; set; }
+        
+        public void AdjustTime(TimeSpan amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
