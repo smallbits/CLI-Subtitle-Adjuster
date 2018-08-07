c1245ccd723e289f4c23b9696410576bd3e4a465
smallbits <mircamilanov95@gmail.com>
2018-08-07 15:25:24 +0200

    Subtitle factory created and srtsubtitle namespace updated

---
+++ SubtitlesLibrary/SubtitleFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitlesLibrary
+{
+    static class SubtitleFactory
+    {
+        public static Subtitle Create(string extension, string input)
+        {
+            switch (extension.ToLower())
+            {
+                case "srt":
+                    return new SrtSubtitle(input);
+                default:
+                    throw new ArgumentException("File extension not recognized");
+            }
+        }
+    }
+}
--- SubtitlesLibrary/Subtitles/SrtSubtitle.cs
+++ SubtitlesLibrary/Subtitles/SrtSubtitle.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SubtitlesLibrary.Subtitles
+namespace SubtitlesLibrary
 {
     class SrtSubtitle : Subtitle
     {
