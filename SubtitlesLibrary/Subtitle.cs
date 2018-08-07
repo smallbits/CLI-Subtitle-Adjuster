fedcf15d05c4c5911cce647ab3d5df79a433ec2d
smallbits <mircamilanov95@gmail.com>
2018-08-07 14:54:46 +0200

    Base subtitle

---
+++ SubtitlesLibrary/Subtitle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitlesLibrary
+{
+    class Subtitle
+    {
+        private List<SubtitleLine> subtitleLines = new List<SubtitleLine>();
+
+        public Subtitle(string input)
+        {
+            throw new NotImplementedException();
+        }
+        public void Adjust()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
